2f29626027ad1872560da829f26b161a34c47dc9
Ankur_Ghosh <ankur.ghosh@intglobal.com>
2025-11-25 10:16:48 +0530

    addingMoreComponents

--- helpdesk-frontend/src/App.jsx
+++ helpdesk-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Button} from "./components/ui/Button";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,9 @@ function App() {
   return (
     <>
       <h1>This is my Application</h1>
+      <Button variant={"ghost"}>Click me</Button>
+      
+      
     </>
   )
 }
